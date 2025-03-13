f1640c8d4b6f21bf663aaa169a1fe04cec4953c8
Šárka F. <sarka.flidrova@email.cz>
2025-03-13 18:05:43 +0100

    Add basic router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Suspense } from 'react';
+import { ReactNode, Suspense } from 'react';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
-const App = () => {
+interface Props {
+  children?: ReactNode;
+}
+
+const App = ({ children }: Props) => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div>Hello World!</div>
+      <div>{children}</div>
+      <Outlet />
     </Suspense>
   );
 };
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>This is a home page.</div>;
+};
+
+export default HomePage;
